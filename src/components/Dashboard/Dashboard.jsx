cdb3f6c7893cdcb94eaf7bafb2743f5dedebaae2
emilianopp <emilianopp550@gmail.com>
2022-01-02 18:43:49 -0500

    add loading states

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import content from 'content/content.json'
+import AnalysisHook from 'components/factories/AnalysisHook';
+function Dashboard({searchKeys}) {
+    let analysis = [];
+    let watching = []
+    searchKeys?.map((key) => { 
+        let tmp = {}
+        tmp[key.id] = key.sel
+        watching.push(tmp) 
+    })
+    content.analysisCalls.map((item) => {
+        analysis.push(AnalysisHook(item,watching));
+    });
 
-function Dashboard() {
+
+  
+ 
+
     return (
         <div>
-            
+           {console.log(analysis[0])}
         </div>
     )
 }
 
 export default Dashboard
+ 
