460d7831e9c221e091e1024697dd25c5002ca0a0
emilianopp <emilianopp550@gmail.com>
2022-01-07 18:01:58 -0500

    switch to second relesae

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, componentDidMount } from "react";
-import { Container, Button, Spinner } from "react-bootstrap";
-import "styles/master.scss";
+import React, { useState } from "react";
+import { Container, Button, Spinner, Row, Col } from "react-bootstrap";
 import content from "content/content.json";
 import NonDependant from "./factories/NonDependant";
 import Dependant from "./factories/Dependant";
@@ -8,49 +7,71 @@ import NonDependantDropdowns from "./menu/NonDependantDropdowns";
 import DependantDropdowns from "./menu/DependantDropdowns";
 import Dashboard from "./Dashboard/Dashboard";
 import completedCalls from "./api/completedCalls";
+import Intro from "./Intro/Intro";
+import Navigation from "./Navigation/Navigation";
+import DashboardMain from "./Dashboard/DashboardMain";
+let navBarHeight= 12
+let contentHeight = 100 - navBarHeight
+
+let styles = {
+  dropdown: {
+    margin: "auto",
+    padding: "10px",
+  },
+  Col: {
+    textAlign: "center",
+  },
+  ContainerStyles: {
+    width: "100vw",
+    height: "auto",
+    minWidth: "100vw",
+    minHeight: "auto",
+    padding: "0",
+    margin: "0",
+    color: content.colors.black
+  },
+  RowStyles: {
+    width: "100%",
+    height: "auto",
+    minWidth: "auto",
+    padding: "0",
+    margin: "0",
+  },
+
+  ColStyles: {
+    padding: "0",
+    margin: "0",
+  },
+  NavRow: {
+    height: `${navBarHeight}vh`
+  },
+  DashboardRow:{
+    height: `${contentHeight}vh`
+  }
+};
 
 function Main() {
   const [submit, setSubmit] = useState(false);
-  let dependantManager = [];
-  let nonDependantManager = [];
+ 
 
-  content.nonDependantDropdowns.map((item) => {
-    nonDependantManager.push(NonDependant(item));
-  });
-
-  content.dependantDropdowns.map((item) => {
-    let depState = nonDependantManager.find(
-      (dep) => dep.id === item.dependancyId
-    );
-    dependantManager.push(Dependant(item, depState.state, depState.sel));
-  });
-
-
-  const handleClick = () => setSubmit(true);
   return (
-    <Container className="page-wrapper">
-      {completedCalls(
-        [...dependantManager, ...nonDependantManager]) ? (
-          <>
-            {" "}
-            <NonDependantDropdowns menuContent={nonDependantManager} />
-            {dependantManager?.map((seearchKey) => (
-              <DependantDropdowns searchKey={seearchKey} />
-            ))}
-            <Button onClick={handleClick}> submit</Button>
-            {submit && (
-              <Dashboard
-                searchKeys={[ ...dependantManager, ...nonDependantManager ]}
-              />
-            )}
-          
-          </>
-        ) : (
-          <div className="SpinnerBox">
-            <Spinner className="Spinner" animation="border" />
-          </div>
-        )
-      }
+    <Container style={styles.ContainerStyles}>
+      <Row style = {styles.NavRow}> 
+      <Navigation setSubmit = {setSubmit}/>
+      </Row>
+      {!submit && (
+        <>
+          <Intro setSubmit= {setSubmit} />
+
+        </>
+      )}
+      
+
+      {submit && (
+        <Row style = {styles.DashboardRow}>
+        <DashboardMain contentHeight = {contentHeight}/>
+        </Row>
+      )}
     </Container>
   );
 }
